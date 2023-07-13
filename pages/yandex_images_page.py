1cb503205a0b928d7138014bd1ff774709eb10bb
Ken760 <49761163+Ken760@users.noreply.github.com>
2023-07-13 15:28:26 +0300

    add yandex images page

--- pages/yandex_images_page.py
+++ pages/yandex_images_page.py
@@ -21,17 +21,51 @@ class ImagesPage(BasePage):
         sleep(1)
         images_link = self.browser.find_element(*YandexImagesLocators.YANDEX_IMAGE_LINK)
         images_link.click()
-        sleep(5)
 
     def checking_current_url(self):
         """Проверка перехода на url"""
         assert "https://yandex.ru/images/" in self.browser.current_url, 'Некорректный url'
 
-    # def switch_to_page(self):
-    #     """Переключение на другое окно браузера"""
-    #     tabs = self.browser.window_handles  # список отрывшихся вкладок
-    #     self.browser.switch_to.window(tabs[1])
-    #
-    # def checking_current_url(self):
-    #     """Проверка перехода на url"""
-    #     assert "https://yandex.ru/images/" in self.browser.current_url, 'Некорректный url'
+    def selecting_theme_images(self):
+        """Открытие первой категории"""
+        images_theme = self.browser.find_elements(*YandexImagesLocators.SELECTING_IMAGES_THEME)
+        selecting_images_theme = images_theme[0].click() # Выбор темы изображения
+        sleep(1)
+
+    def title_check_in_search(self):
+        """Проверка названия категории в поиске"""
+        title_theme = self.browser.find_element(*YandexImagesLocators.TITLE_THEME).text
+        input_text = self.browser.find_element(*YandexImagesLocators.TEXT_INPUT).get_attribute('value')
+        assert title_theme in input_text, "Название категории не отображается в поле поиска"
+
+    def click_images(self):
+        """Проверка на открытие первой картинки"""
+        assert self.is_element_present(*YandexImagesLocators.SELECTING_IMAGES), \
+            "Картинка отсутствует, страница не открылась"
+        images = self.browser.find_elements(*YandexImagesLocators.SELECTING_IMAGES)
+        selecting_images = images[0]  # Выбор картинки
+        selecting_images.click()
+        sleep(1)
+
+    def check_images(self):
+        """Проверка на совпадение изображений"""
+        img_url = self.browser.current_url
+        self.button_next_images()
+        self.button_previous_images()
+        assert self.browser.current_url in img_url, "Изображения не совпадают"
+
+    def button_next_images(self):
+        """Кнопка выбора следущей картинки"""
+        assert self.is_element_present(*YandexImagesLocators.NEXT_IMAGE_BUTTON), \
+            "Кнопка следущего картинки отсутствует"
+        next_button = self.browser.find_element(*YandexImagesLocators.NEXT_IMAGE_BUTTON)
+        next_button.click()
+        sleep(1)
+
+    def button_previous_images(self):
+        """Кнопка выбора предыдущей картинки"""
+        assert self.is_element_present(*YandexImagesLocators.PREVIOUS_IMAGE_BUTTON), \
+            "Кнопка предыдущего картинки     отсутствует"
+        prev_button = self.browser.find_element(*YandexImagesLocators.PREVIOUS_IMAGE_BUTTON)
+        prev_button.click()
+        sleep(1)
