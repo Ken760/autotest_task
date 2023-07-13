ac244908bfd151912bcf5b988b3f202b383cdab6
Ken760 <49761163+Ken760@users.noreply.github.com>
2023-07-13 15:28:35 +0300

    Update tests.py

--- tests.py
+++ tests.py
@@ -1,19 +1,40 @@
 from pages.yandex_search_page import SearchPage
+from pages.yandex_images_page import ImagesPage
 
 import allure
 
 
 @allure.title("Поиск в яндексе")
 def test_search_in_yandex(browser):
-    yandex_main_page = SearchPage(browser)
-    yandex_main_page.go_to_site('https://ya.ru/')
+    yandex_search_page = SearchPage(browser)
+    yandex_search_page.go_to_site('https://ya.ru/')
     with allure.step("Проверка наличия поиска"):
-        yandex_main_page.search_in_yandex('Тензор')
+        yandex_search_page.search_in_yandex('Тензор')
     with allure.step("Проверка на то, что появилась таблица с подсказками (suggest)"):
-        yandex_main_page.checking_suggest()
+        yandex_search_page.checking_suggest()
     with allure.step("При нажатии Enter появляется таблица результатов поиска"):
-        yandex_main_page.click_on_the_search()
+        yandex_search_page.click_on_the_search()
     with allure.step("Проверка, что появилась страница результатов поиска"):
-        yandex_main_page.checking_search_results()
+        yandex_search_page.checking_search_results()
     with allure.step("Проверка на то, что первая ссылка ведет на tensor.ru"):
-        yandex_main_page.checking_link_results()
+        yandex_search_page.checking_link_results()
+
+
+@allure.title("Картинки на яндексе")
+def test_yandex_images(browser):
+    yandex_images_page = ImagesPage(browser)
+    yandex_images_page .go_to_site('https://ya.ru/')
+    with allure.step("Проверка, что кнопка меню присутсвтует на странице"):
+        yandex_images_page .checking_button_menu()
+    with allure.step('Проверка, что меню открылось и перешло по ссылке'):
+        yandex_images_page.open_menu_select_images()
+    yandex_images_page.switch_to_page()
+    with allure.step("Проверка на то, что перешли на url https://yandex.ru/images/"):
+        yandex_images_page.checking_current_url()
+    with allure.step("Проверка что категория открылась, в строке поиске верный текст"):
+        yandex_images_page.click_theme_images()
+    with allure.step("Открыть 1 картинку, проверить что открылась"):
+        yandex_images_page.click_images()
+    with allure.step("При нажатии кнопки вперед картинка изменяется,"
+                     "При нажатии кнопки назад картинка изменяется на то же изображение"):
+        yandex_images_page.check_images()
